6634accef8f558a36a3d02ab59e18d0e2477f7e0
Náthally <nathallyfernandes@ufmg.br>
2026-09-03 18:48:54 -0300

    Commit 6: Moving method get_charge from Rental to Book

--- model.py
+++ model.py
@@ -7,26 +7,31 @@ class Book:
     def __init__(self, title: str, price_code: int):
         self.title = title
         self.price_code = price_code
+    # Método movido para cá
+    def get_charge(self, days_rented: int) -> float:
+        amount = 0
+        if self.price_code == Book.REGULAR:
+            amount += 2
+            if days_rented > 2:
+                amount += (days_rented - 2) * 1.5
+        elif self.price_code == Book.NEW_RELEASE:
+            amount += days_rented * 3
+        elif self.price_code == Book.CHILDREN:
+            amount += 1.5
+            if days_rented > 3:
+                amount += (days_rented - 3) * 1.5
+        return amount
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
         self.book = book
         self.days_rented = days_rented
 
+    # Método agora apenas delega
     def get_charge(self) -> float:
-        amount = 0
-        if self.book.price_code == Book.REGULAR:
-            amount += 2
-            if self.days_rented > 2:
-                amount += (self.days_rented - 2) * 1.5
-        elif self.book.price_code == Book.NEW_RELEASE:
-            amount += self.days_rented * 3
-        elif self.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if self.days_rented > 3:
-                amount += (self.days_rented - 3) * 1.5
-        return amount
+        return self.book.get_charge(self.days_rented)
 
+    # get_frequent_renter_points continua igual por enquanto...
     def get_frequent_renter_points(self) -> int:
         points = 1
         if self.book.price_code == Book.NEW_RELEASE and self.days_rented > 1:
